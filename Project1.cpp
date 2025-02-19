5a160cef0e6a405b60aeec1604e29c427ff33e07
deha tecaoglu <dehateja@gmail.com>
2025-02-18 22:26:04 -0500

    we lit

---
+++ Project1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int drawing()
+{
+	cout << "Ascl Art:" << endl;
+	cout << "    ___ " << endl;
+	cout << "   //_\\\\_" << endl;
+	cout << " .''\\    ''." << endl;
+	cout << " /          \\" << endl;
+	cout << "|           \\_" << endl;
+	cout << "|       ,--.-.)" << endl;
+	cout << " \\     /  o \\o\\ " << endl;
+	cout << " /\\/\\  \\    /_/" << endl;
+	cout << "  (_.   `--'__)" << endl;
+	cout << "   |     .-'  \\" << endl;
+	cout << "   |  .-'.     )" << endl;
+	cout << "   | (  _/--.-'" << endl;
+	cout << "   |  `.___.'" << endl;
+	cout << "         (" << endl;
+return 0;
+}
--- Project2.cpp
+++ Project2.cpp
@@ -11,27 +11,28 @@ int main()
 	id = 123;
 	rate = 15.25;
 	worked = 25;
-
-	cout << "Salary Calculator:" << endl;
+	cout << "Salary Calculator" << endl;
 	cout << "*****************" << endl;
+	cout << "" << endl;
 	cout << "Enter Employee ID:" << id << endl;
-	cout << "Enter Hourly Rate:" << rate << endl;
-	cout << "Enter Hours Worked:" << worked << endl;
+	cout << "Enter Hourly Rate:";
+	cin >> rate;
+	cout << "Enter Hours Worked:";
+	cin >> worked;
 
+	cout << "" << endl;
 	cout << "Employee Information:" << endl;
 	cout << "---------------------" << endl;
 	cout << "Employee ID:" << id << endl;
 	cout << fixed << setprecision(3);
-	cout << "Hourly Rate:";
-	cin >> rate;
+	cout << "Hourly Rate:" << rate <<endl;
 	cout << fixed << setprecision(3);
-	cout << "Hours Worked:";
-	cin >> worked;
+	cout << "Hours Worked:" << worked << endl;
+	
 
 	double average = (rate * worked);
 	cout << fixed << setprecision(3);
 	cout << "Gross Pay:" << average << endl;
-	
 
-	//joe mama
-}
+
+	}
